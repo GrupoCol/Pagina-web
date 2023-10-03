5b9b93aad6b0ff93f1f3fb350febf3a582c65f5c
GrupoCol <vcabrera@grupocolchagua.cl>
2023-10-03 13:04:02 -0300

    martes 03

--- js/numeros.js
+++ js/numeros.js
@@ -1,31 +1,11 @@
-let counter = 0;
-const targetSection = document.getElementById('targetSection');
-const counterElement = document.getElementById('counter');
-
-window.addEventListener('scroll', () => {
-  if (isElementInViewport(targetSection)) {
-    startCounter();
-    window.removeEventListener('scroll', onScroll);
-  }
-});
-
-function startCounter() {
-  const interval = setInterval(() => {
-    counterElement.innerText = counter;
+let count = document.querySelector(".count");
+let counter= 600;
+setInterval(()=>{
+  if(counter< 3500){
     counter++;
-    if (counter > 3500) {
-      clearInterval(interval);
-      counterElement.innerText = '3500';
-    }
-  }, 1);
-}
-
-function isElementInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
+    count.innerHTML= counter;
+  }else {
+    clearInterval(interval)
+  }
+  console.log('test');
+},0.1)
