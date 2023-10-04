44eab7166d5acd1ef09652de7e9ec18af850274b
GrupoCol <vcabrera@grupocolchagua.cl>
2023-10-04 14:14:37 -0300

    04-aalmuerzo

--- js/numeros.js
+++ js/numeros.js
@@ -1,41 +1,51 @@
-const containers = document.querySelectorAll(".containers span");
-const container = document.querySelector(".containers");
-
-let activated = false;
-
-window.addEventListener("scroll", ()=>{
-if(
-  pageYOffset > container.offsetTOP - container.offsetHeight -200
-  && activated== false
-){
-  container.forEach(container =>{
-    container.innerText=0;
-
-    let count =0;
-
-    function updateCount(){
-      const target =parseInt(container.dataset.count);
-      if(count < target){
-        count++;
-        console.innerText= count;
-        setTimeout(updateCount,10);
-      }else{
-        container.innerText = target;
-      }
+function updateCounter(start, end, counterElement) {
+  let counter = start;
+  const intervalId = setInterval(() => {
+    counterElement.innerText = counter;
+    counter++;
+    if (counter > end) {
+      clearInterval(intervalId);
+      counterElement.innerText = end;
     }
-    updateCount();
-    activated= true;
-  });
-} else if(
-  pageYOffset <= container.offsetTOP - container.offsetHeight -500
-  || pageXOffset === 0
-  && actividad ===true
- ){
-  containers.forEach(container => {
-    container.innerText = 0;
-  });
-  activated =false ; 
- }
-
+  }, 1);
 }
-)
+
+function startCounterOnScroll(start, end, counterElement, targetSection) {
+  let counting = false;
+
+  function checkStartCounter() {
+    if (isElementInViewport(targetSection) && !counting) {
+      counting = true;
+      counter = start;
+      updateCounter(start, end, counterElement);
+    }
+  }
+
+  window.addEventListener('scroll', checkStartCounter);
+}
+
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+const counter1Element = document.getElementById('counter1');
+const targetSection1 = document.getElementById('targetSection1');
+startCounterOnScroll(0, 5, counter1Element, targetSection1);
+
+const counter2Element = document.getElementById('counter2');
+const targetSection2 = document.getElementById('targetSection2');
+startCounterOnScroll(0, 10, counter2Element, targetSection2);
+
+const counter3Element = document.getElementById('counter3');
+const targetSection3 = document.getElementById('targetSection3');
+startCounterOnScroll(0, 350, counter3Element, targetSection3);
+
+const counter4Element = document.getElementById('counter4');
+const targetSection4 = document.getElementById('targetSection4');
+startCounterOnScroll(0, 3500, counter4Element, targetSection4);
---
+++ js/titulos.js
@@ -0,0 +1,22 @@
+const titleElement = document.getElementById('title');
+
+function animateTitle() {
+  titleElement.classList.add('shown');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Check if animation has already been shown
+  const animationShown = sessionStorage.getItem('animationShown');
+  if (!animationShown) {
+    setTimeout(() => {
+      animateTitle();
+      // Set a flag to indicate animation has been shown
+      sessionStorage.setItem('animationShown', 'true');
+    }, 5000);
+  } else {
+    // If animation has already been shown, display the title immediately
+    animateTitle();
+  }
+});
+
+
